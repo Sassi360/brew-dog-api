7ca58fddc79d91774c04c5b40bcb0c43c59c2b80
Mohamed Sassi <mohamed@msassi.dev>
2023-11-15 09:52:44 -0500

    refactor: move BeerItem into it own component + add linting

---
+++ components/BeerItem.vue
@@ -0,0 +1,56 @@
+<script setup>
+defineProps({
+  beer: Object,
+})
+</script>
+
+<template>
+  <div class="list-item">
+    <img :src="beer.image_url" alt="Image of {{ beer.name }}">
+    <h2>{{ beer.name }}</h2>
+    <p>{{ beer.tagline }}</p>
+    <p>{{ beer.description }}</p>
+    <p>ABV: {{ beer.abv }}</p>
+    <p>IBU: {{ beer.ibu }}</p>
+    <p v-show="beer.isLactose" class="warning">
+      Contains Lactose
+    </p>
+    <p v-show="beer.isDryHopped" class="highlight">
+      Dry Hopped
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.list-item {
+  border-radius: 10px;
+  border: 1px solid #ccc;
+  padding: 20px;
+  text-align: center;
+}
+
+.list-item img {
+  height: auto;
+  width: 100%;
+}
+
+.list-item h2 {
+  margin: 10px 0;
+}
+
+.list-item p {
+  margin: 5px 0;
+}
+
+.warning {
+  color: #e63946;
+  font-weight: bold;
+  text-decoration: underline;
+}
+
+.highlight {
+  background-color: #ffc300;
+  color: #000000;
+  padding: 5px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,32 @@
-<template>
-  <span v-if="pending">Loading...</span>
-  <span v-else-if="error">Error: {{ error.message }}</span>
+<script setup>
+import { fetchBeers } from '~/api/beers'
 
-  <div class="list">
-    <template v-for="beer in beers" :key="beer.id">
-      <div class="list-item">
-        <img :src="beer.image_url" alt="Image of {{ beer.name }}" />
-        <h2>{{ beer.name }}</h2>
-        <p>{{ beer.tagline }}</p>
-        <p>{{ beer.description }}</p>
-        <p>ABV: {{ beer.abv }}</p>
-        <p>IBU: {{ beer.ibu }}</p>
-        <p v-show="beer.isLactose" class="warning">Contains Lactose</p>
-        <p v-show="beer.isDryHopped" class="highlight">Dry Hopped</p>
-      </div>
-    </template>
+const { data: beers, pending, error, refresh } = useAsyncData('beers', fetchBeers)
+</script>
+
+<template>
+  <div v-if="pending">
+    Loading...
   </div>
 
-  <button @click="refresh">Refresh Data</button>
-</template>
+  <div v-else-if="error">
+    Error: {{ error.message }}
+  </div>
 
-<script setup>
-  import { fetchBeers } from "~/api/beers";
-  const { data: beers, pending, error, refresh } = useAsyncData("beers", fetchBeers);
-</script>
+  <div v-else class="list">
+    <BeerItem v-for="beer in beers" :key="beer.id" :beer="beer" />
+  </div>
+
+  <button @click="refresh">
+    Refresh Data
+  </button>
+</template>
 
 <style scoped>
 .list {
-  display: grid;
-  gap: 20px;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  padding: 20px;
-}
-
-.list-item {
-  border-radius: 10px;
-  border: 1px solid #ccc;
-  padding: 20px;
-  text-align: center;
-}
-
-.list-item img {
-  height: auto;
-  width: 100%;
-}
-
-.list-item h2 {
-  margin: 10px 0;
-}
-
-.list-item p {
-  margin: 5px 0;
-}
-
-.warning {
-  color: #e63946;
-  font-weight: bold;
-  text-decoration: underline;
-}
-
-.highlight {
-  background-color: #ffc300;
-  color: #000000;
-  padding: 5px;
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    padding: 20px;
 }
 </style>
